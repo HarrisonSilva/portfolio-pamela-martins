0333242457d7270dd260eb5af14341b99746f3ed
Harrison <Harrison.couto@outlook.com>
2024-01-16 20:39:31 -0300

    inserindo algumas informaçoes no portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,12 +7,17 @@ export default function page() {
   return (
     <div>
         <div className={styles.container}>
-          <Image src={photo} width='330' height='350' alt='foto da pamela' />
-          <p>Sou graduanda encontro-me no 5° <br />
+          <Image className={styles.img} src={photo} width='330' height='350' alt='foto da pamela' />
+          <div className={styles.containerAbout}>
+            <h1>Psicóloga Pamela Martins</h1>
+            <h4>Especialista em Psicoterapia CRP 00/0000</h4>
+          <p>Sou graduada encontro-me no 5° <br />
              periodo do curso de Psicologia <br />
              e atuo na area de treinamento <br />
              e desenvolvimento no setor de RH</p>
+          </div>
         </div>
+        <h1 style={{display: 'none'}}>Formação Acadêmica</h1>
     </div>
   )
 }
