abef4d32dab628ab4f3f56c1ce1c7b8e6fe442a7
Harrison <Harrison.couto@outlook.com>
2024-01-23 21:42:07 -0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,51 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import img from '../assets/images/logo.png';
 import styles from '../styles/header.module.css';
 import Link from 'next/link';
 
 export default function Header() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
   const toogleMenuDisplay = () => {
-    const body = document.body.clientWidth
     const menuContainer = document.querySelector('#menu-container');
-    const logo = document.querySelector('#logo');
-    const computedStyle = window.getComputedStyle(menuContainer);
-   
-    if (computedStyle.display === 'none' && body <= 775) {
+
+    if (!menuVisible) {
       menuContainer.style.display = 'flex';
-      logo.style.display = 'none'
-    } else if (menuContainer.style.display = 'flex' && body > 775) {
-      menuContainer.style.display = 'flex';
-      logo.style.display = 'flex';
-  }
-}
+    } else {
+      menuContainer.style.display = 'none';
+    }
+
+    setMenuVisible(!menuVisible);
+  };
 
   return (
     <>
-    <div onClick={toogleMenuDisplay} className={styles.menu}>
+      <div onClick={toogleMenuDisplay} className={styles.menu}>
         <div></div>
         <div></div>
         <div></div>
       </div>
-    <header className={styles.container}>
-      <div id='logo' className={styles.containerLogo}>
-        <div className={styles.containerTitle}>
-          <Link href='/'>
-            <Image priority src={img} width='150' height='150' alt='' />
-          </Link>
-          <div>
-            <p className={styles.professional}>Pisicóloga</p>
-            <h2 className={styles.name}>Pamela Martins</h2>
-            <p>Sua vida começa onde sua zona de conforto termina</p>
+      <header className={styles.container}>
+        <div id='logo' className={styles.containerLogo}>
+          <div className={styles.containerTitle}>
+            <Link href='/'>
+              <Image priority src={img} width='150' height='150' alt='' />
+            </Link>
+            <div>
+              <p className={styles.professional}>Pisicóloga</p>
+              <h2 className={styles.name}>Pamela Martins</h2>
+              <p>Sua vida começa onde sua zona de conforto termina</p>
+            </div>
+          </div>
+          <div className={styles.timeContainer}>
+            <h4>Horário de atendimento</h4>
+            <p>Segunda a Sexta: 14:00 - 17:00</p>
           </div>
         </div>
-        <div className={styles.timeContainer}>
-          <h4>Horário de atendimento</h4>
-          <p>Segunda a Sexta: 14:00 - 17:00</p>
-        </div>
-      </div>
-      <ul onClick={toogleMenuDisplay} id='menu-container' className={styles.containerNaviBar}>
+      </header>
+      <ul id='menu-container' className={styles.containerNaviBar}>
         <li>
           <Link href='/'>início</Link>
         </li>
@@ -59,7 +59,6 @@ export default function Header() {
           <Link href='/contato'>Contato</Link>
         </li>
       </ul>
-    </header>
     </>
   );
 }
